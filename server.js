1f90f1ede6ad67ce9d9bc4319684c9f1c3baa555
vedavyas1993 <76591323+vedavyas1993@users.noreply.github.com>
2023-10-24 03:35:32 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data");
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id == req.params.id);
+  res.send(singleChat);
+});
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(`server started on port ${port}`);
+});
