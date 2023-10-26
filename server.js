369b03562c4874f34d6d7545706ca3c643fdd991
vedavyas-m <76591323+vedavyas1993@users.noreply.github.com>
2023-10-26 13:14:39 +0530

    added login and sign up and error handling functionality

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
+
 const { chats } = require("./data");
 const connectDB = require("./config/db");
-const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleWare");
 const app = express();
+
+app.use(express.json());
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectDB();
@@ -11,15 +17,10 @@ app.get("/", (req, res) => {
   res.send("running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
-app.get("/api/chat/:id", (req, res) => {
-  //   console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id == req.params.id);
-  res.send(singleChat);
-});
 app.listen(port, (error) => {
   if (error) {
     console.log(error);
